539080679aaa8dd47c5e3916a3fb3e8415f74e84
Simon Berger <simon@siku2.io>
2025-12-04 23:35:00 +0000

    temp: fmt

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -8,11 +8,7 @@
 
         <template #right>
           <UTooltip :text="$t('notifications')" :shortcuts="['N']">
-            <UButton
-              color="neutral"
-              variant="ghost"
-              square
-            >
+            <UButton color="neutral" variant="ghost" square>
               <UChip color="error" inset>
                 <UIcon name="i-lucide-bell" class="size-5 shrink-0" />
               </UChip>
